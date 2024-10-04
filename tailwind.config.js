0f2ee884da72b1423b491d5e379c1ebe4d086f5e
Keith Valdeo <119393041+johnkarlkeith@users.noreply.github.com>
2024-10-04 08:01:54 +0800

    Hotfix 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        slidein: {
+          from: {
+            opacity: "0",
+            transform: "translateY(-10px)",
+          },
+          to: {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+      },
+      animation: {
+        slidein: "slidein 1s ease 300ms",
+      },
+      animation: {
+        slidein300: "slidein 1s ease 300ms",
+        slidein500: "slidein 1s ease 500ms",
+        slidein700: "slidein 1s ease 700ms",
+      },
+    },
   },
   plugins: [
     require('tailwindcss'),
